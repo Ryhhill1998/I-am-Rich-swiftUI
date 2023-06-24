ab9cc42116fe9556625326c4d323235def8b6ea5
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-24 14:25:50 +0100

    Initial Commit

---
+++ I am Rich swiftUI/I_am_Rich_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_swiftUIApp.swift
+//  I am Rich swiftUI
+//
+//  Created by Ryan Henzell-Hill on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
